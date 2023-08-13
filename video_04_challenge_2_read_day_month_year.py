92ed69654dc9c0e36610da0b9258bd5193fd081e
Darlaine Gomes Bezerra <101824263+Darlaine-GB@users.noreply.github.com>
2023-08-12 21:10:47 -0300

    Add files via upload

---
+++ video_04_challenge_2_read_day_month_year.py
@@ -0,0 +1,7 @@
+#Challenge 2 - Read the day, month and year of birthday of someone and inform the full date.
+
+year = input ("Please, inform the year (yyyy) of your birthday:")
+month = input ("Please, inform the month (mm) of your birthday:")
+day = input ("Please, inform the day (dd) of your birthday:")
+
+print (f"You was born on {month}/{day}/{year}")
